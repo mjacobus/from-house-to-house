898eef516d23a20a9a2258bc906a01c2fb6cece9
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2013-05-04 15:51:31 -0300

    Bare rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FromHouseToHouse::Application.config.secret_token = 'a088748cffe6a2f4a62498e86b8161847bb61252429b536c34ca7e89ac444d8968e20ee8aeb90efd55b7c8708a500577a13ad4cf7f41887fdc94d3af42ca9451'
