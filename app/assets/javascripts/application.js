26b091cd33506485d46f2714cfe71590575d9493
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2013-05-04 16:14:03 -0300

    Installed some gems
    rspec
    factory_girl
    twitter bootstrap
    shoulda matchers
    simplecov
    backbone.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/from_house_to_house
 //= require_tree .
