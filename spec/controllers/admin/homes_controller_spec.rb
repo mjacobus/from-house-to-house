83c8a0760861aac1e980da7b24c3971459cb7eed
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-06-16 23:26:06 -0300

    Fixed spec

--- spec/controllers/admin/homes_controller_spec.rb
+++ spec/controllers/admin/homes_controller_spec.rb
@@ -7,7 +7,7 @@ describe Admin::HomesController do
     let(:param_name)  { :home  }
     let(:collection_name)  { :homes  }
     let(:valid_user)  { create(:user, admin: true) }
-    let(:valid_attributes)  { attributes_for(:home) }
+    let(:valid_attributes)  { attributes_for(:home).merge(territory_id: create(:territory).id.to_s, area_id: create(:area).id.to_s) }
   end
 
   it_behaves_like 'a login protected resource'
